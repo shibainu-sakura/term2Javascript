e8b863fbc601be1d57e2f2522e5991dde7b901a9
Takahiro Suzuki <takahirosuzuki.k010@icloud.com>
2017-09-02 13:47:50 +1000

    initial commit

---
+++ scope.js
@@ -0,0 +1,63 @@
+//scope.js   node.scope.js to test on terminal
+//scope  is ttelling us where the variables exist
+
+// // #step1-1
+// var a = 1;
+//
+// function foo(){
+//   var a = 5;
+//   console.log(a);
+// }
+//
+// function bar(){
+//   var a =
+//   console.log(a);
+// }
+
+
+// // #step1-2
+// var a = 1;
+//
+// function foo(){
+//   var a = 6;
+//   console.log(a);
+// }
+//
+// function bar(){
+//   var a = 1;
+//   console.log(a);
+// }
+
+
+// // #step1-3
+// foo();
+// bar();
+// console.log(a);
+//
+// var a = 1;
+//
+// function foo(){
+//   var a = 6;
+//   console.log(a);
+// }
+//
+// function bar(){
+//   var a = 2;
+//    a = 5;
+//   console.log(a);
+// }
+//
+// foo();
+// bar();
+// console.log(a);
+
+
+// // #step2-1
+// function foo() {
+//   var a = 6;
+//   return a;
+// }
+//
+// var b = foo();
+//
+// console.log(b);
